c738b53120bbb040553994fba0364a4fb90534f5
unknown <nathan.regner@Albert-22.mtchs.org>
2015-05-27 11:06:07 -0600

    Reservation Form Logic
    Partial implementation, doesn't quite work yet

--- App_Code/Reservation.cs
+++ App_Code/Reservation.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
+using System.Web.Script.Serialization;
 using WebMatrix.Data;
 
 /// <summary>
@@ -9,12 +12,60 @@ using WebMatrix.Data;
 /// </summary>
 public class Reservation
 {
+    private int clientId;
+    private int regionId;
+    private DateTime startDate;
+    private DateTime endDate;
+
+    SqlConnection connection = new SqlConnection("Server=IISProject.mtchs.org;Database=PlanetInc;Trusted_Connection=Yes");
     public Boolean isReserved;
 
-	public Reservation(Database db, int clientId, int regionId, DateTime startDate, DateTime endDate)
-	{
-	    Database.Open("IISProject");
+    public Reservation(int clientId, int regionId, DateTime startDate, DateTime endDate)
+    {
+        this.clientId = clientId;
+        this.regionId = regionId;
+        this.startDate = startDate;
+        this.endDate = endDate;
+
+        connection.Open();
+        isReserved = checkIfReserved();
+
+        if (!isReserved)
+        {
+            reserve();
+        }
+    }
+
+    private void reserve()
+    {
+        
+    }
+
+    private Boolean checkIfReserved()
+    {
+        // TODO: this is bullshit
+        SqlCommand cmd = new SqlCommand(null, connection);
+
+        cmd.CommandText = "SELECT COUNT(*) " +
+            "FROM Reservations " +
+            "WHERE '@start' BETWEEN StartDate AND EndDate OR " +
+            "'@end' BETWEEN StartDate AND EndDate OR " +
+            "'@start' = StartDate AND '@end' = EndDate;";
+
+        SqlParameter startParam = new SqlParameter("@start", SqlDbType.VarChar);
+        SqlParameter endParam = new SqlParameter("@end", SqlDbType.VarChar);
+        startParam.Value = startDate.ToShortDateString();
+        endParam.Value = endDate.ToShortDateString();
+
+        cmd.Parameters.Add(startParam);
+        cmd.Parameters.Add(endParam);
+
+        isReserved = (int) cmd.ExecuteScalar() == 0;
+
+        connection.Close();
+
+        return isReserved;
+    }
 
 
-	}
 }
